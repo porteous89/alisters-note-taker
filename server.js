676d82a3badf24d1f449d92c9e4b6405d4f0f172
Alister Porteous <alisterporteous@hotmail.com>
2023-02-23 15:43:21 -0500

    added html routes, middleware and connection to api routes

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const path = require('path');
-const api = require('');
+const noteRoutes = require('./routes/noteRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/api", noteRoutes);
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+
+app.listen(PORT, () => console.log('Now listening'));
